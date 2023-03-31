ff2dbcdf597daa3e10a36bd98bb8a888a9395be9
Hulyamr13 <122914464+Hulyamr13@users.noreply.github.com>
2023-03-31 19:17:00 +0300

    updates

---
+++ djangoproject/phonebook/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from djangoproject.phonebook.views import landing_page, create_phone_number
+
+
+urlpatterns =[
+    path('', landing_page, name='landing-page'),
+    path('new-contact', create_phone_number, name='new-contact')
+]
---
+++ djangoproject/phonebook/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect
+from djangoproject.phonebook.models import Contact
+
+
+def get_all_phones():
+    return Contact.objects.all
+
+def landing_page(request):
+    all_phone = get_all_phones()
+    context = {
+        "contacts": all_phone
+    }
+    return render(request, "phonebook/index.html", context)
+
+
+def create_phone_number(request):
+    name = request.POST['name']
+    number = request.POST['number']
+    contact = Contact(
+        name=name,
+        number=number
+    )
+    contact.save()
+    all_phones = get_all_phones()
+    context = {'contacts': all_phones}
+    return render(request, 'phonebook/index.html', context)
